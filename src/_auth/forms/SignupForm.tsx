b33b7265a2b91493de93919517678feda017c1f3
VojtechBarnet <vojtech.barnet@me.com>
2024-01-13 01:20:22 +0100

    First commit
    Just structure for my new project!

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Components
+import { Button } from '@/components/ui/button'
+
+function SignupForm() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default SignupForm
